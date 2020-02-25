70c687d6461c31febd0256f54a5f01ee7297638c
nikita <nicitazaletin@yandex.ru>
2020-02-25 10:59:26 +0300

    Laba1BankFix

--- src/Laba1.java
+++ src/Laba1.java
@@ -1,60 +1,49 @@
 import java.util.Scanner;
-import java.util.Stack;
+
 public class Laba1 {
 
-        private static final char cr = '(';
-        private static final char cl = ')';
-        private static final char sr = '[';
-        private static final char sl = ']';
-        private static final char fr = '{';
-        private static final char fl = '}';
+    public static void main(String[] args) {
 
-        public static boolean isCorrectBrackets(String value) {
-            Stack<Character> stack = new Stack<Character>();
-            char[] array = value.toCharArray();
-            for(char c : array) {
-                if((c == cl || c == sl || c == fl) && stack.empty()) {
-                    return false;
-                }
+        System.out.print("Введите через пробел количество купюр различного номинала: 1000, 500, 100, 30 \n");
+        Scanner in = new Scanner(System.in);
+        int arr[] = new int[4];
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        int sum = a * 1000 + b * 500 + c * 100 + d * 30;
 
-                if(c == cr || c == sr || c == fr) {
-                    stack.push(c);
-                }
-
-                if((c == cl || c == sl || c == fl) && !stack.empty()) {
-                    char t = stack.peek();
-                    switch(c) {
-                        case ')':
-                            if(t == '(') stack.pop();
-                            break;
-                        case ']':
-                            if(t == '[') stack.pop();
-                            break;
-                        case '}':
-                            if(t == '{') stack.pop();
-                            break;
-                    }
-                }
+        System.out.print("Введите сумму, которую желаете получить \n");
+        int x = in.nextInt();
+        if (x > sum || (x % 10 != 0)) {
+            System.out.print("Данную сумму невозможно выдать. Обратитесь в другой банкомат.\n");
+        } else {
+            while (x % 100 != 0 && x > 0 && d > 0) {
+                x -= 30;
+                arr[3]++;
+                d--;//добавил уменьшение d
             }
-            if(stack.empty()) {
-                return true;
-            } else {
-                return false;
+            while (a > 0 && x - 1000 >= 0) {
+                x -= 1000;
+                a--;
+                arr[0]++;
             }
-        }
-
-        public static void main(String[] args) {
-            // тестируем
-            Scanner in = new Scanner(System.in);
-            System.out.print("Введите строку скобок: ");
-            String str=in.nextLine();
-            if(isCorrectBrackets(str)) {
-                System.out.println("Скобки расставлены правильно");
-            } else {
-                System.out.println("Скобки расставлены неправильно");
+            while (b > 0 && x - 500 >= 0) {
+                x -= 500;
+                b--;
+                arr[1]++;
             }
-
-
+            while (c > 0 && x - 100 >= 0) {
+                x -= 100;
+                c--;
+                arr[2]++;
+            }
+            if (x == 0) {
+                System.out.print(arr[0] + " купюр(ы) наминалом 1000, " + arr[1] + " купюр(ы) наминалом 500, \n"
+                        + arr[2] + " купюр(ы) наминалом 100, " + arr[3] + " купюр(ы) наминалом 30");
+            }
+            else System.out.print("Данную сумму невозможно выдать. Обратитесь в другой банкомат.\n");//добавил эту строчку
         }
 
     }
+}
